fb7663da9a2359bc69f7780af86eeff27ffd67d2
ZenekeZene <hectorvillarm@gmail.com>
2022-06-17 20:38:20 +0200

    Mobile camera.

--- src/modules/camera/mobile/MobileCamera.js
+++ src/modules/camera/mobile/MobileCamera.js
@@ -1,4 +1,5 @@
 import { useRef } from 'react'
+import './MobileCamera.scss'
 
 const MobileCamera = () => {
   const imageSrc = useRef(null)
@@ -7,27 +8,29 @@ const MobileCamera = () => {
     imageSrc.current = window.URL.createObjectURL(this.files[0])
   }
 
-  return (<>
-    { /* The `label` is attached to the hidden file input */ }
-    <label for="cameraFileInput">
+  return (
+    <section className="mobile-camera">
+      { /* The `label` is attached to the hidden file input */ }
+      <label for="cameraFileInput">
 
-      { /* The hidden file `input` for opening the native camera */ }
-      <input
-        id="cameraFileInput"
-        type="file"
-        accept="image/*"
-        capture="environment"
-        onChange={ handleChange }
-        style={{ display: 'none' }}
+        { /* The hidden file `input` for opening the native camera */ }
+        <input
+          id="cameraFileInput"
+          type="file"
+          accept="image/*"
+          capture="environment"
+          onChange={ handleChange }
+          style={{ display: 'none' }}
+        />
+      </label>
+
+      { /* displays the picture uploaded from the native camera */ }
+      <img
+        alt="Espacio disponible para albergar retratos del usuario"
+        src={ imageSrc }
       />
-    </label>
-
-    { /* displays the picture uploaded from the native camera */ }
-    <img
-      alt="Espacio disponible para albergar retratos del usuario"
-      src={ imageSrc }
-    />
-  </>)
+    </section>
+  )
 }
 
 export default MobileCamera
