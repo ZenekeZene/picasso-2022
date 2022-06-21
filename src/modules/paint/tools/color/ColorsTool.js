9c57011eff8d3899a3347d6b1a3dbf37d0f61b5d
ZenekeZene <hectorvillarm@gmail.com>
2022-06-21 11:38:51 +0200

    Swiper in ColorsTool.

--- src/modules/paint/tools/color/ColorsTool.js
+++ src/modules/paint/tools/color/ColorsTool.js
@@ -1,47 +1,62 @@
 import { useEffect, useState } from 'react'
+import { Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import colors from './colors'
+import 'swiper/css'
+import 'swiper/css/pagination'
 import './ColorsTool.scss'
 
-export const colors = [
-  '#008f7a',
-  '#845ec2',
-  '#d65db1',
-  '#ff6f91',
-  '#ffc75f',
-  'rgb(252, 222, 192)',
-  'rgb(27, 37, 52)',
-]
+const swiperConfig = {
+  pagination: { clickable: false },
+  spaceBetween: 5,
+  slidesPerView: 1,
+  allowTouchMove: true
+}
+
+const ColorsSlide = ({ colors, currentColor, setCurrentColor }) => (
+  <ol className="color-tool__swatches">
+    { colors.map((color, index) => (
+      <li
+        key={ `color-${index}` }
+        className={ currentColor === color ? '--selected': '' }
+        onClick={ () => { setCurrentColor(color) }}
+      ><span className="circle" style={{ backgroundColor: color }}></span></li>
+    ))}
+  </ol>
+)
 
 const ColorsTool = ({ onColor, setSectionsVisibled, areSectionsVisibled }) => {
-  const [currentColor, setCurrentColor] = useState(colors[0])
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentColor, setCurrentColor] = useState(colors[0][0])
 
   useEffect(() => {
     onColor(currentColor)
   }, [currentColor, onColor])
 
+  const items = colors.map((color) => (
+    <ColorsSlide
+      colors={ color }
+      currentColor={ currentColor }
+      setCurrentColor={ setCurrentColor }
+    />
+  ))
+
   return (
-    <div className="color-tool not-hover">
+    <article className="color-tool not-hover">
       { !areSectionsVisibled && (
         <span className="color-tool__selected"
           style={{ backgroundColor: currentColor }}
           onClick={ () => { setSectionsVisibled(true) }}
         ></span>
       )}
-      { areSectionsVisibled && (
-        <ol className="color-tool__swatches">
-          { colors.map((color, index) => (
-            <li
-              key={ `color-${index}` }
-              className={ currentColor === color ? '--selected': '' }
-              onClick={ () => {
-                setCurrentColor(color)
-                setSectionsVisibled(false)
-              }}
-              style={{ backgroundColor: color }}
-            ></li>
-          ))}
-        </ol>
-      )}
-    </div>
+      <Swiper
+        modules={[ Pagination ]}
+        { ...swiperConfig }
+        className={ !areSectionsVisibled ? '--invisible' : '' }
+      >
+        { items.map((item, index) => <SwiperSlide key={ index }>{ item }</SwiperSlide>) }
+      </Swiper>
+    </article>
   )
 }
 
---
+++ src/modules/paint/tools/color/colors.js
@@ -0,0 +1,31 @@
+export const colors = [
+  '#008f7a',
+  '#845ec2',
+  '#d65db1',
+  '#ff6f91',
+  '#ffc75f',
+  'rgb(252, 222, 192)',
+  'rgb(27, 37, 52)',
+]
+
+const colorsSecondary = [
+  '#5d91d6',
+  '#5dd6cd',
+  '#5dd65e',
+  '#d6c65d',
+  '#d6855d',
+  '#d65d9e',
+  '#d65d5d',
+]
+
+const colorsTerciary = [
+  '#e5e9ef',
+  '#c9ccd0',
+  '#aeb0b3',
+  '#8c8e90',
+  '#67686a',
+  '#353637',
+  '#191a1a'
+]
+
+export default [colors, colorsSecondary, colorsTerciary]
